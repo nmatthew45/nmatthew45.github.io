afe3f75e23a7757ebf8cfc14fd38c05cbcfa0b94
nmatthew45 <nmatthew45@gmail.com>
2021-05-16 00:57:17 -0700

    third commit

---
+++ src/components/Contact.js
@@ -0,0 +1,23 @@
+import Container from "react-bootstrap/Container";
+import resume from "../images/resume.pdf";
+
+function Contact() {
+  return (
+    <Container fluid style={{ textAlign: "center" }}>
+      <a href={resume}>
+        <i class="fas fa-file contactIcon"></i>
+      </a>
+      <a href="mailto:nmatthew45@gmail.com">
+        <i class="fas fa-envelope contactIcon"></i>
+      </a>
+      <a href="https://www.github.com/nmatthew45">
+        <i class="fab fa-github contactIcon"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/matthewn45/">
+        <i class="fab fa-linkedin-in contactIcon"></i>
+      </a>
+    </Container>
+  );
+}
+
+export default Contact;
