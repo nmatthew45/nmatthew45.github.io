182029d120b0b7060fa3fe569fcbb7b86c6c96e1
nmatthew45 <nmatthew45@gmail.com>
2021-05-16 00:52:33 -0700

    cleanup unused imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
-import resume from "./images/resume.pdf";
 import picture from "./images/me.jpeg";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Nav from "react-bootstrap/Nav";
 import Image from "react-bootstrap/Image";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -16,20 +11,7 @@ function App() {
       </div>
       <h1>Matthew Nguyen</h1>
       <p>React Developer based in Westminster, California</p>
-      <Container className="resumeButton" fluid>
-        <a href={resume}>
-          <i class="fas fa-file contactIcon"></i>
-        </a>
-        <a href="mailto:nmatthew45@gmail.com">
-          <i class="fas fa-envelope contactIcon"></i>
-        </a>
-        <a href="https://www.github.com/nmatthew45">
-          <i class="fab fa-github contactIcon"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/matthewn45/">
-          <i class="fab fa-linkedin-in contactIcon"></i>
-        </a>
-      </Container>
+      <Contact />
     </div>
   );
 }
