58338ed8c89c5e7c2a2349a9212945d2d89a0ae2
nmatthew45 <nmatthew45@gmail.com>
2021-05-17 18:05:00 -0700

    cleanup components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import "./css/Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      © {new Date().getFullYear()} Matthew Nguyen. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Info.js
@@ -0,0 +1,12 @@
+import "./css/Info.css";
+
+function Info() {
+  return (
+    <div>
+      <h1>Matthew Nguyen</h1>
+      <p>React Developer based in Westminster, California</p>
+    </div>
+  );
+}
+
+export default Info;
