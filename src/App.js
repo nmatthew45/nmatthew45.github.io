7543983049cc71bcc1f9712c4adcf4d6a05c76d8
nmatthew45 <37429968+nmatthew45@users.noreply.github.com>
2021-12-02 21:13:22 -0800

    update Switch to Routes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Navigationbar from "./components/Navbar";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="appStyle">
         <Navigationbar />
-          <Route exact path="/" component={Home} />
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
           {/* <Route path="/projects" component={Projects} /> */}
+        </Routes>
         <Footer />
       </div>
     </Router>
